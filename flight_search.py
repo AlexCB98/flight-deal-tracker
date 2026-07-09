d9b438f550233d9b2edb5a31450f76058c81799e
Cocioaba Alexandru Bogdan <lexyus22@gmail.com>
2026-07-09 11:36:25 +0200

    Add fake flight prices for local testing

--- flight_search.py
+++ flight_search.py
@@ -3,8 +3,18 @@ from flight_data import FlightData
 class FlightSearch:
 
     def search_flight(self, origin_airport, destination_airport):
+
+        if destination_airport == 'PAR':
+            price = 120
+        elif destination_airport == 'BER':
+            price = 80
+        elif destination_airport == 'TYO':
+            price = 700
+        else:
+            price = 999
+
         flight_data = FlightData(
-            price=120,
+            price=price,
             origin_airport=origin_airport,
             destination_airport=destination_airport,
             out_date='2026-08-10',
