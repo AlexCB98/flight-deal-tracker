bae4c69da8a15777923f904d747df0e53d516e53
Cocioaba Alexandru Bogdan <lexyus22@gmail.com>
2026-07-09 10:58:13 +0200

    Add local flight data structure

--- main.py
+++ main.py
@@ -1,5 +1,6 @@
 from data_manager import DataManager
 from notification_manager import NotificationManager
+from flight_data import FlightData
 
 data_manager = DataManager()
 notification_manager = NotificationManager()
@@ -9,3 +10,17 @@ data = data_manager.get_destination_data()
 notification_manager.send_notification("Test alert")
 
 print(data)
+
+flight = FlightData(
+    price=120,
+    origin_airport="MUC",
+    destination_airport="PAR",
+    out_date="2026-08-10",
+    return_date="2026-08-17",
+)
+
+print(flight.price)
+print(flight.origin_airport)
+print(flight.destination_airport)
+print(flight.out_date)
+print(flight.return_date)
