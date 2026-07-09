78a6d3c4785df505bc8eb98f51175c5586fd35cc
Cocioaba Alexandru Bogdan <lexyus22@gmail.com>
2026-07-09 11:28:59 +0200

    Connect local flight search with destination data

--- flight_search.py
+++ flight_search.py
@@ -1,2 +1,14 @@
+from flight_data import FlightData
+
 class FlightSearch:
-    pass
+
+    def search_flight(self, origin_airport, destination_airport):
+        flight_data = FlightData(
+            price=120,
+            origin_airport=origin_airport,
+            destination_airport=destination_airport,
+            out_date='2026-08-10',
+            return_date='2026-08-17',
+        )
+
+        return flight_data
--- main.py
+++ main.py
@@ -1,26 +1,19 @@
 from data_manager import DataManager
 from notification_manager import NotificationManager
-from flight_data import FlightData
+from flight_search import FlightSearch
 
+
+flight_search = FlightSearch()
 data_manager = DataManager()
-notification_manager = NotificationManager()
+notification = NotificationManager()
 
 data = data_manager.get_destination_data()
 
-notification_manager.send_notification("Test alert")
+for destination in data:
+    searched_flight = flight_search.search_flight('MUC', destination['iataCode'])
 
-print(data)
-
-flight = FlightData(
-    price=120,
-    origin_airport="MUC",
-    destination_airport="PAR",
-    out_date="2026-08-10",
-    return_date="2026-08-17",
-)
-
-print(flight.price)
-print(flight.origin_airport)
-print(flight.destination_airport)
-print(flight.out_date)
-print(flight.return_date)
+    if searched_flight.price < destination['lowestPrice']:
+        notification.send_notification(
+            f'Low price alert! Flight from {searched_flight.origin_airport} '
+            f'to {searched_flight.destination_airport} costs {searched_flight.price}€.'
+        )
