2ca05117481e7b5fb4e856b4afe860f195c2ae4a
Cocioaba Alexandru Bogdan <lexyus22@gmail.com>
2026-07-10 10:50:38 +0200

    Move flight prices to class initialization

--- flight_search.py
+++ flight_search.py
@@ -3,8 +3,8 @@ from flight_data import FlightData
 
 class FlightSearch:
 
-    def search_flight(self, origin_airport, destination_airport):
-        prices = {
+    def __init__(self):
+        self.prices = {
             'PAR': 120,
             'BER': 80,
             'TYO': 700,
@@ -14,12 +14,15 @@ class FlightSearch:
             'AMS': 130,
         }
 
-        price = prices.get(destination_airport, 999)
+    def search_flight(self, origin_airport, destination_airport, destination_city):
+
+        price = self.prices.get(destination_airport, 999)
 
         flight_data = FlightData(
             price=price,
             origin_airport=origin_airport,
             destination_airport=destination_airport,
+            destination_city=destination_city,
             out_date='2026-08-10',
             return_date='2026-08-17',
         )
