8d09a4e58bfb0e9835a84c465a89833242e5aec1
Cocioaba Alexandru Bogdan <lexyus22@gmail.com>
2026-07-10 10:34:39 +0200

    Replace flight price conditions with dictionary lookup

--- flight_search.py
+++ flight_search.py
@@ -4,15 +4,17 @@ from flight_data import FlightData
 class FlightSearch:
 
     def search_flight(self, origin_airport, destination_airport):
+        prices = {
+            'PAR': 120,
+            'BER': 80,
+            'TYO': 700,
+            'LON': 150,
+            'ROM': 110,
+            'MAD': 140,
+            'AMS': 130,
+        }
 
-        if destination_airport == 'PAR':
-            price = 120
-        elif destination_airport == 'BER':
-            price = 80
-        elif destination_airport == 'TYO':
-            price = 700
-        else:
-            price = 999
+        price = prices.get(destination_airport, 999)
 
         flight_data = FlightData(
             price=price,
