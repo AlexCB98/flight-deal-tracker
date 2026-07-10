79bad817a3c725c27b1b0506a88b12c2f6f20a5a
Cocioaba Alexandru Bogdan <lexyus22@gmail.com>
2026-07-10 10:04:25 +0200

    Save flight deals to JSON

--- notification_manager.py
+++ notification_manager.py
@@ -1,4 +1,23 @@
+import json
+
+
 class NotificationManager:
 
     def send_notification(self, message):
-        print(message)
+
+        try:
+            with open('flight_deals.json', 'r') as flight_deals:
+                data = json.load(flight_deals)
+        except FileNotFoundError:
+            data = []
+        except json.JSONDecodeError:
+            data = []
+
+        new_deal = {
+            'flight_deal': message,
+        }
+
+        data.append(new_deal)
+
+        with open('flight_deals.json', 'w') as flight_deals:
+            json.dump(data, flight_deals, indent=4)
